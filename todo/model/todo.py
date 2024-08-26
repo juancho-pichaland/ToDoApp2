e9e327d296bfb532e463221e156fa4c3c84ec3e3
juanph02 <juanpabloarroyave1@hotmail.com>
2024-08-26 13:42:21 -0500

    si

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,45 @@
 # TODO: Add code here
+class Todo:
+    def _init_(self, code_id: int, title: str, description: str):
+        self.code_id = code_id
+        self.title = title
+        self.description = description
+        self.completed = False
+        self.tags: list[str] = []
+
+    def mark_completed(self):
+        self.completed = True
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def _str_(self):
+        return f"{self.code_id} - {self.title}"
+
+
+class TodoBook:
+    def _init_(self):
+        self.todos: dict[int, Todo] = {}
+
+    def add_todo(self, title: str, description: str) -> int:
+        code_id = len(self.todos) + 1
+        new_todo = Todo(code_id, title, description)
+        self.todos[code_id] = new_todo
+        return code_id
+
+    def pending_todos(self) -> list[Todo]:
+        return [todo for todo in self.todos.values() if not todo.completed]
+
+    def completed_todos(self) -> list[Todo]:
+        return [todo for todo in self.todos.values() if todo.completed]
+
+    def tags_todo_count(self) -> dict[str, int]:
+        tag_count = {}
+        for todo in self.todos.values():
+            for tag in todo.tags:
+                if tag in tag_count:
+                    tag_count[tag] += 1
+                else:
+                    tag_count[tag] = 1
+        return tag_count
